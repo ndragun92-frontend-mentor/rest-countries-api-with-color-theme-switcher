9cb68f728214a035d3190ca69434f4b4e134833e
ndragun92 <nemanjadragun92@gmail.com>
2023-02-21 11:09:56 +0100

    chore: update README.md

--- app.vue
+++ app.vue
@@ -2,13 +2,30 @@
   <NuxtLayout>
     <Html lang="en" :data-route-path="$route?.path">
       <Head>
-        <Title>Template</Title>
+        <Title
+          >Frontend Mentor | REST Countries API with color theme switcher</Title
+        >
         <Meta name="language" content="en" />
-        <Meta name="og:title" content="Template" />
-        <Meta name="twitter:title" content="Template" />
-        <Meta name="description" content="Template" />
-        <Meta name="og:description" content="Template" />
-        <Meta name="twitter:description" content="Template" />
+        <Meta
+          name="og:title"
+          content="Frontend Mentor | REST Countries API with color theme switcher"
+        />
+        <Meta
+          name="twitter:title"
+          content="Frontend Mentor | REST Countries API with color theme switcher"
+        />
+        <Meta
+          name="description"
+          content="Challenge is to integrate with the REST Countries API to pull country data and display it like in the designs"
+        />
+        <Meta
+          name="og:description"
+          content="Challenge is to integrate with the REST Countries API to pull country data and display it like in the designs"
+        />
+        <Meta
+          name="twitter:description"
+          content="Challenge is to integrate with the REST Countries API to pull country data and display it like in the designs"
+        />
         <Meta name="og:image" content="/favicon.png" />
         <Meta name="twitter:image" content="/favicon.png" />
         <Meta name="og:site_name" content="/favicon.png" />
